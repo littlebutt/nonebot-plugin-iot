a8ced8281c5f2be4404d72a217ef7a536978c569
littlebutt <luogan199686@gmail.com>
2022-10-12 09:58:00 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     python_requires=">=3.8",
     name='nonebot-plugin-iot',
-    version='1.0.0',
+    version='1.1.0',
     packages=['iot', 'iot.devices'],
     install_requires=['nonebot2>=2.0.0a1', 'nonebot-adapter-onebot>=2.1.0'],
     url='https://github.com/littlebutt/nonebot-plugin-iot',
